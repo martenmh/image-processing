9fdff0ae2a428a6eda0fc2eb5accca058c28357c
martenmh <Marten@homma-clan.nl>
2019-09-11 19:12:56 +0200

    Reworked setContrast and added new feature

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
+#define COLOR image.at<Vec3b>(Point(x,y))
+
+enum FocusedMode{
+	BlackWhite,
+	Contrast,
+	Negative,
+	Brightness,
+	None
+};
+
+// Global variables 
+Mat image;
+std::string asciImage;
+Mat resetImage;
+FocusedMode mode = None;
+int threshold;
+int value;
+std::string imageString;
+
 
 void setColor(Vec3b &color, int val){
 	for(int i = 0; i<3; ++i){
@@ -16,204 +35,199 @@ void addToColor(Vec3b &color, int val){
 	}
 }
 
+void reset(FocusedMode m){
+	// Mat image = imread(imageString, 1 );
+	// Mat resetImage = imread(imageString, 1 );
+	::mode = m;
+	int threshold = 127;
+	int value = 0;
+}
 
-
-void setBlackWhite(Mat &image, int boundary){
+Mat setBlackWhite(Mat image, int boundary){
 	// Set pixel black
+	boundary *= 3;
 	for(int x = 0; x<image.rows; ++x){
-    	for(int y = 0; y<image.cols; ++y){
+		for(int y = 0; y<image.cols; ++y){
 
-    		Vec3b &color = image.at<Vec3b>(Point(x,y));
+    		// Vec3b color = image.at<Vec3b>(Point(x,y));
     		// Set pixel
-			if((color[0]+color[1]+color[2]) > (boundary * 3)){
-				setColor(color, 255);
+
+			if((COLOR[0]+COLOR[1]+COLOR[2]) > (boundary)){
+				setColor(COLOR, 255);
 			}
 			// Set pixel white
 			else{
-				setColor(color, 0);
+				setColor(COLOR, 0);
 			}
 		}
 	}
+	return image;
 }
 
 // Make light pixels lighter and dark pixels darker
-void setContrast(Mat &image, int boundary, int amount){
-		for(int x = 0; x<image.rows; ++x){
-    	for(int y = 0; y<image.cols; ++y){
-    		Vec3b &color = image.at<Vec3b>(Point(x,y));
-    		
-	if((color[0]+color[1]+color[2]) > (boundary * 3)){
-		addToColor(color, amount);
+Mat setContrast(Mat image, int boundary, int amount){
+	boundary *= 3;
+	for(int x = 0; x<image.rows; ++x){
+		for(int y = 0; y<image.cols; ++y){
+			Vec3b &color = image.at<Vec3b>(Point(x,y));
+
+			if((COLOR[0]+COLOR[1]+COLOR[2]) > (boundary)){
+				addToColor(color, amount);
+			}
+			else{
+				addToColor(color, -amount);
+			}
+		}
 	}
-	else{
-		amount = -amount;
-		addToColor(color, amount);
+	return image;
+}
+
+
+// Previous setContrast version that created cool distortion/noise effect
+Mat setDistorion(Mat image, int boundary, int amount){
+	for(int x = 0; x<image.rows; ++x){
+		for(int y = 0; y<image.cols; ++y){
+			Vec3b &color = image.at<Vec3b>(Point(x,y));
+			
+			if((color[0]+color[1]+color[2]) > (boundary * 3)){
+				addToColor(color, amount);
+			}
+			else{
+				amount = -amount;
+				addToColor(color, amount);
+			}
+		}
 	}
+	return image;
 }
-}
-}
+
 
 // Set all pixels brighter or darker 
-void setBrightness(Mat &image, int brightness){
-		for(int x = 0; x<image.rows; ++x){
-    	for(int y = 0; y<image.cols; ++y){
-    		Vec3b &color = image.at<Vec3b>(Point(x,y));
-    		
-	addToColor(color, brightness);
-}
-}
-}
-
-
-void setNegative(Mat &image){
+Mat setBrightness(Mat image, int brightness){
 	for(int x = 0; x<image.rows; ++x){
-    	for(int y = 0; y<image.cols; ++y){
-    		Vec3b &color = image.at<Vec3b>(Point(x,y));
+		for(int y = 0; y<image.cols; ++y){
+			Vec3b &color = image.at<Vec3b>(Point(x,y));
+
+			addToColor(color, brightness);
+		}
+	}
+	return image;
+}
+
+
+Mat setNegative(Mat image){
+	for(int x = 0; x<image.rows; ++x){
+		for(int y = 0; y<image.cols; ++y){
+			Vec3b &color = image.at<Vec3b>(Point(x,y));
 
 			color[0] = -color[0];
 			color[1] = -color[1];
 			color[2] = -color[2];
 		}
 	}	
+	return image;
 }
 
 
-
-
-enum FocusedMode{
-	BlackWhite,
-	Contrast,
-	Negative,
-	Brightness,
-	None
-};
-
 int main(int argc, char *argv[])
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    
-    Mat image;
-    // Parse char to int
-    
+	if ( argc != 2 )
+	{
+		printf("usage: DisplayImage.out <Image_Path>\n");
+		return -1;
+	}    
 
-    image = imread( argv[1], 1 );
-    Mat resetImage = imread( argv[1], 1 );
-    // Go over every pixel and set it either black or white depending on the threshold 
-    // for(int x = 0; x<image.rows; ++x)
-    // 	for(int y = 0; y<image.cols; ++y)
-    // 		setBlackWhite(image.at<Vec3b>(Point(x,y)), threshold);
-    
-    //std::string asciImage;
-    FocusedMode mode = None;
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
+	std::string imageString = argv[1];
+	image = imread(imageString, 1);
+	int threshold = 127;
+	int value = 0;
+	resetImage = imread(imageString, 1);
 
-   if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
+	namedWindow("Display Image", WINDOW_AUTOSIZE );
 
-    bool looping = true;
-    int threshold = 127;
-    int value = 127;
-    do{
+	if ( !image.data || !resetImage.data )
+	{
+		printf("No image data \n");
+		return -1;
+	}
+
+	bool looping = true;
 
 
-	    // for(int x = 0; x<image.rows; ++x){
-	    // 	for(int y = 0; y<image.cols; ++y){
-	    // 		setContrast(image.at<Vec3b>(Point(x,y)), 127, threshold);
-	    // 	}
-	    // }
+	do{
 
-	 //setBrightness(image.at<Vec3b>(Point(x,y)), threshold);
+		std::cout << mode << std::endl;
 
-	   std::cout << mode << std::endl;
-	   
-	   std::cout << "Value: " << value << "\nThreshold: " << threshold << std::endl;
-	    switch(mode){
-	    	case BlackWhite:
-	    		setBlackWhite(image, value);
-	    	break;
-	    	case Contrast:
-	    
-	    		setContrast(image, threshold, value);
-			    
+		std::cout << "Value: " << value << "\nThreshold: " << threshold << std::endl;
+		switch(mode){
+			case BlackWhite:
+			image = setBlackWhite(resetImage, threshold);
+			break;
+			case Contrast:
 
-	
-	    		
-	    	break;
-	    	case Brightness:
-	    
-			    setBrightness(image, value);
+			image = setContrast(resetImage, threshold, value);
 
-		
-	    	break;
-	    	case Negative:
 
-	    		setNegative(image);
-	   
-	    	break;
-	    	case None:
-	    	break;
+
+
+			break;
+			case Brightness:
+
+			image = setBrightness(resetImage, value);
+
+
+			break;
+			case Negative:
+
+			image = setNegative(resetImage);
+
+			break;
+			case None:
+			break;
 		}
 
-	    
 
-	    
-	    imshow("Display Image", image);
 
-	    
 
-	    auto k = waitKey(0);
-	    std::cout << "you pressed: " << k << std::endl;
-	    
+		imshow("Display Image", image);
+
+
+
+		auto k = waitKey(0);
+		std::cout << "you pressed: " << k << std::endl;
+
 	    if(k == 27){	// On escape close program
 	    	looping = false;
 	    }
 	    if(k == 99){ 	// On c 
-	    	image = resetImage;
-		    mode = Contrast;
-		    value = 0;
-		    threshold = 127;
+	    	reset(Contrast);
+
+
 	    }	
 	    if(k == 118){ 	// On b
-	    	image = resetImage;
-		    mode = Brightness;
-		    value = 0;
-		    threshold = 127;
+	    	reset(Brightness);
+
+
 	    }	
 	    if(k == 98){ 	// On v
-	    	image = resetImage;
-		    mode = BlackWhite;
-		    value = 0;
-		    threshold = 0;
+	    	reset(BlackWhite);
+
 	    }	
 	    if(k == 110){ 	// On n
-	    	image = resetImage;
-		    mode = Negative;
-		    value = 0;
-		    threshold = 0;
+	    	reset(Negative);
+
 	    }	
 
-	    if(k == 81) threshold += 10;	// On arrow left
-	    if(k == 83) threshold -= 10;	// On arrow right
-	    if(k == 82) value += 10;	// On arrow top
-	    if(k == 84) value -= 10;	// On arrow down	
+	    if(k == 83) threshold += 1;	// On arrow left
+	    if(k == 81) threshold -= 1;	// On arrow right
+	    if(k == 82) value += 1;	// On arrow top
+	    if(k == 84) value -= 1;	// On arrow down	
 	    if(k == 114){				// On r
-	    	
-	    	image = resetImage;
-	    	value = 0;
-		    threshold = 0;
-		    mode = None;
+	    	reset(None);
 	    }
 
 	} while(looping);
-    
-    
 
-    return 0;
+
+	return 0;
 }
